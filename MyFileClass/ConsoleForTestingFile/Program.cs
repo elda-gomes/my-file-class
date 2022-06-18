ac7e2ebc87ae1fb916884f0201670b7adc68ad3c
elda-gomes <elda.dimm@gmail.com>
2022-06-18 13:00:29 -0300

    Implementing my own File class just for fun. Just reading a file for now.

---
+++ MyFileClass/ConsoleForTestingFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyFileClass;
+using System;
+
+namespace ConsoleForTestingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing File.ReadAllLines: ");
+            Console.WriteLine();
+
+            var lines = File.ReadAllLines("testFile.txt");
+
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("Press <enter> to test File.ReadAllText.");
+            Console.ReadLine();
+
+            Console.WriteLine("Testing File.ReadAllText: ");
+            Console.WriteLine();
+
+            var text = File.ReadAllText("testFile.txt");
+            Console.WriteLine(text);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyFileClass/MyFileClass/File.cs
@@ -0,0 +1,63 @@
+﻿using My.Generic.List;
+using System.IO;
+using System.Text;
+
+namespace MyFileClass
+{
+    public static class File
+    {
+        private const int _bufferSizeInBytes = 1024;
+
+        //Doing the thing using my own List type, buffer, checking the end of file and converting bytes in string "manually" and using try/finally in order to close file
+        public static string[] ReadAllLines(string filePath, Encoding encoding)
+        {
+            var fileStream = new FileStream(filePath, FileMode.Open);
+            var buffer = new byte[_bufferSizeInBytes];
+            var numberOfBytesRead = -1;
+            var allLines = new List<string>();
+
+            try
+            {
+                while (numberOfBytesRead != 0)
+                {
+                    numberOfBytesRead = fileStream.Read(buffer, 0, _bufferSizeInBytes);
+                    allLines.Add(encoding.GetString(buffer, 0, numberOfBytesRead));
+                }
+            }
+            finally
+            {
+                fileStream.Close();
+            }        
+
+            return allLines.ToArray();
+        }
+
+        public static string[] ReadAllLines(string filePath)
+        {
+            return File.ReadAllLines(filePath, System.Text.Encoding.Default);
+        }
+
+        //Doing the thing using StreamReader and using()
+        public static string ReadAllText(string filePath, Encoding encoding)
+        {
+            var text = new StringBuilder();
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            using (var streamReader = new StreamReader(fileStream, encoding))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    var line = streamReader.ReadLine();
+                    text.AppendLine(line);
+                }
+            }
+
+            return text.ToString();
+        }
+
+        public static string ReadAllText(string filePath)
+        {
+            return File.ReadAllText(filePath, Encoding.Default);
+        }
+    }
+}
